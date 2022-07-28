d7178a7eee47ee4972dacefac7a33d9978b4026f
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2022-07-28 18:48:48 +0800

    Update diy.sh

--- devices/at91_sama5/diy.sh
+++ devices/at91_sama5/diy.sh
@@ -4,5 +4,5 @@ shopt -s extglob
 
 SHELL_FOLDER=$(dirname $(readlink -f "$0"))
 
-sed -i 's/DEFAULT_PACKAGES +=/DEFAULT_PACKAGES += autocore-arm luci-app-cpufreq fdisk lsblk btrfs-progs block-mount blkid parted dosfstools e2fsprogs pv losetup uuidgen automount/' target/linux/at91/Makefile
+sed -i 's/DEFAULT_PACKAGES +=/DEFAULT_PACKAGES += autocore-arm luci-app-cpufreq fdisk lsblk btrfs-progs block-mount blkid parted dosfstools e2fsprogs pv losetup uuidgen automount tune2fs resize2fs/' target/linux/at91/Makefile
 
